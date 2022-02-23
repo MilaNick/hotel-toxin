bb8b7694cdd7ddaf81dfd762f6dd4cdbff0c7d4a
Mila Zolotova <l.zolotova@yahoo.com>
2022-02-24 02:09:11 +0300

    init dist

--- src/index.js
+++
@@ -1,4 +0,0 @@
-import '@styles/index.scss'
-import $ from "jquery";
-
-$('.block').html('Начинаем собирать шаблон');
--- src/user.js
+++
@@ -1,3 +0,0 @@
-import $ from "jquery";
-
-$('.footer').html('Шаблон webpack собран');
